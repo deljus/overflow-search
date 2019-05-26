faf77e0e2c9f97b1f4b0e61c1a0135123c90371c
deljus <musindelus@gmail.com>
2019-05-26 20:38:21 +0300

    hotfix

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,30 +1,4 @@
-
-async function oldXHR(url, params){
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open(params.method || 'get', url);
-            xhr.onload = function () {
-                if (this.status >= 200 && this.status < 300) {
-                    resolve(xhr.response);
-                } else {
-                    reject({
-                        status: this.status,
-                        statusText: xhr.statusText
-                    });
-                }
-            };
-            xhr.onerror = function () {
-                reject({
-                    status: this.status,
-                    statusText: xhr.statusText
-                });
-            };
-            xhr.send();
-        });
-}
-
-
-async function fetchRequest(url, params){
+async function request(url, params) {
     try{
         let response = await fetch(url, params);
         const ans = await response.json();
@@ -33,13 +7,6 @@ async function fetchRequest(url, params){
     }catch (error) {
         throw error;
     }
-}
-
-async function request(url, params) {
-    if (fetch) {
-        return await fetchRequest(url, params)
-    }
-    return await oldXHR(url, params)
 }
 
 export default request;
